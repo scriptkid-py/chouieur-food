167408afb716b804b8573d5e36566ce79e829507
scriptkid-py <r44chr@gmail.com>
2025-10-29 12:09:46 +0100

    Remove H1 with Food & Helado from kitchen dashboard header

--- src/app/kitchen/layout.tsx
+++ src/app/kitchen/layout.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-import { Logo } from '@/components/Logo';
-import Link from 'next/link';
-
 export default function KitchenLayout({
   children,
 }: {
@@ -12,9 +9,6 @@ export default function KitchenLayout({
     <div className="flex min-h-screen flex-col">
       <header className="sticky top-0 z-40 border-b bg-background">
         <div>
-          <Link href="/kitchen">
-            <Logo className="text-foreground" />
-          </Link>
         </div>
       </header>
       <main className="flex-1 bg-muted/30 p-4 sm:p-6 lg:p-8">
