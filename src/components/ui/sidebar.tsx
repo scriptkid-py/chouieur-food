b76c32e945cbc18603c4c02b52b91760e2c5583e
scriptkid-py <r44chr@gmail.com>
2025-11-06 15:53:17 +0100

    Remove sidebar-wrapper class from SidebarProvider

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -133,10 +133,7 @@ const SidebarProvider = React.forwardRef<
                 ...style,
               } as React.CSSProperties
             }
-            className={cn(
-              "group/sidebar-wrapper flex min-h-svh w-full has-[&[data-variant=inset]]:bg-sidebar",
-              className
-            )}
+            className={cn(className)}
             ref={ref}
             {...props}
           >
